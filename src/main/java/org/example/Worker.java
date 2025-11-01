00ab863de5d59ce879dfc9c0455605b682d16d84
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-01 21:49:32 +0400

    add implementation for run method

--- src/main/java/org/example/Worker.java
+++ src/main/java/org/example/Worker.java
@@ -2,15 +2,45 @@ package org.example;
 
 import java.util.concurrent.BlockingQueue;
 
-public class Worker implements Runnable{
+public class Worker implements Runnable {
     private final String name;
     private BlockingQueue<Order> orders;
+    private final Warehouse warehouse;
 
 
-    public Worker(String name) {
+    public Worker(String name, BlockingQueue<Order> orders, Warehouse warehouse) {
         this.name = name;
+        this.orders = orders;
+        this.warehouse = warehouse;
     }
 
     @Override
-    public void run(){}
+    public void run() {
+        try {
+
+            while (true) {
+                Order order = orders.take();
+                if (order.getProduct() == null) {
+                    break;
+                }
+
+                boolean process = warehouse.processOrder(order);
+
+                if (process) {
+                    System.out.println("Worker " + name + " successfully processed order for "
+                            + order.getProduct().getName()
+                            + " " + order.getQuantity()
+                            + "x");
+                }else {
+                    System.out.println("Worker " + name + " failed to process order for "
+                            + order.getProduct().getName()
+                            + " " + order.getQuantity()
+                            + "x");
+                }
+
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
