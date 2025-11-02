f0529359f55ef9d9215ee2d5e395167886b461c3
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-02 17:50:36 +0400

    modified to cleaner structure

--- src/main/java/org/example/Warehouse.java
+++ src/main/java/org/example/Warehouse.java
@@ -1,39 +1,43 @@
-    package org.example;
+package org.example;
 
-    import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
-    public class Warehouse {
-        private final ConcurrentHashMap<Product, Integer> stock = new ConcurrentHashMap<>();
-        private final Analytics analytics;
+public class Warehouse {
+    private final ConcurrentHashMap<Product, Integer> stock = new ConcurrentHashMap<>();
+    private final Analytics analytics;
 
-        public Warehouse(Analytics analytics) {
-            this.analytics = analytics;
-        }
+    public Warehouse(Analytics analytics) {
+        this.analytics = analytics;
+    }
 
-        public void addProduct(Product product, int quantity) {
-            stock.put(product, quantity);
-        }
+    public void addProduct(Product product, int quantity) {
+        stock.put(product, quantity);
+    }
 
-        public ConcurrentHashMap<Product,Integer> getStock(){
-            return stock;
-        }
+    public ConcurrentHashMap<Product, Integer> getStock() {
+        return stock;
+    }
 
-        public boolean processOrder(Order order) {
-            Product product = order.getProduct();
-            int orderQuantity = order.getQuantity();
-            int stockQuantity = stock.getOrDefault(product, 0);
+    public boolean processOrder(Order order) {
+        Product product = order.getProduct();
+        int orderQuantity = order.getQuantity();
+        int stockQuantity = stock.getOrDefault(product, 0);
 
-            //Checks if there is enough quantity in stock to process order or if customer has enough balance
-            if (stockQuantity >= orderQuantity && order.getCustomer().debitBalance(order.getPrice())) {
-                //If TRUE, then subtracts order quantity from stock quantity
-                stock.computeIfPresent(product, (key, quantity) -> quantity - orderQuantity);
-                analytics.addProcessedOrders(order);
-                return true;
-            }
-
+        //Checks if there is enough quantity in stock
+        if (!(stockQuantity >= orderQuantity)) {
             return false;
-
         }
 
+        //Debit customer balance, if rejected return false
+        if (!(order.getCustomer().debitBalance(order.getPrice()))) {
+            return false;
+        }
+
+        //If TRUE, then subtracts order quantity from stock quantity
+        stock.computeIfPresent(product, (key, quantity) -> quantity - orderQuantity);
+        analytics.addProcessedOrders(order);
+        return true;
 
     }
+
+}
