aa4dd0b4f759e26a3164cdebc58df8f910a9189c
shadowoflobster <guraspishvilileqso@gmail.com>
2025-11-02 15:18:55 +0400

    Add analyze method

--- src/main/java/org/example/Analytics.java
+++ src/main/java/org/example/Analytics.java
@@ -3,13 +3,16 @@ package org.example;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 public class Analytics {
     private int orderCount;
     private double profit;
     private ConcurrentHashMap<Product, Integer> top3Products;
     private final List<Order> processedOrders = Collections.synchronizedList(new ArrayList<>());
+    private List<Map.Entry<Product, Integer>> top3Sales;
 
 
     public List<Order> getProcessedOrders() {
@@ -17,11 +20,27 @@ public class Analytics {
     }
 
     //when warehouse processes order successfully, it will be added to this list for later analytics
-    public void addProcessedOrders(Order o){
+    public void addProcessedOrders(Order o) {
         processedOrders.add(o);
     }
 
+    public void analyse() {
+        orderCount = Math.toIntExact(processedOrders.parallelStream().count());
 
+        profit = processedOrders.parallelStream().mapToDouble(Order::getPrice).sum();
 
+        Map<Product, Integer> productSales = processedOrders.parallelStream()
+                .collect(Collectors.toConcurrentMap(
+                        Order::getProduct,
+                        Order::getQuantity,
+                        Integer::sum
+                ));
 
+        top3Sales = productSales.entrySet().stream()
+                .sorted(Map.Entry.<Product, Integer>comparingByValue().reversed())
+                .limit(3)
+                .toList();
+    }
 }
+
+
